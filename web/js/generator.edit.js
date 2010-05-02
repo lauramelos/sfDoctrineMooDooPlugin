c3c269f2fa56ab33cb5990573d3649611c2be14e
RetroFOX <damian.suarez@xifox.net>
2010-05-02 17:02:09 -0300

    add widget DateAndTime. Una bosta

--- web/js/generator.edit.js
+++ web/js/generator.edit.js
@@ -5,4 +5,28 @@ window.addEvent('domready', function () {
   inputsDate.each(function (inputDate) {
     inputDate.addCalendar();
   })
+
+
+  var inputsDateTime = $$('input.input_date-time');
+  inputsDateTime.each(function (inputDate) {
+    inputDate.addCalendar();
+
+    // contruimos la hora
+    console.debug ("inputDate.getNext() -> ", inputDate.getNext());
+
+    inputDate.getNext().addEvents({
+      blur: function (ev) {
+        var dates = inputDate.get('value').split(' ');
+        inputDate.set('value', dates[0] + ' ' + this.get('value'));
+      }
+    })
+
+    new InputMask.Time(inputDate.getNext(), {
+      onError: function(element){
+        element.highlight('#f88');
+      }
+    });
+  })
+
+
 })
