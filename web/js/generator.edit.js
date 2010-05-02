883d698648a761638e263d625fd5152416e47d7e
RetroFOX <damian.suarez@xifox.net>
2010-05-02 14:56:20 -0300

    js

--- web/js/generator.edit.js
+++ web/js/generator.edit.js
@@ -1,7 +1,8 @@
 window.addEvent('domready', function () {
   // add datapicker to input type date
   var inputsDate = $$('input.input_date');
+
   inputsDate.each(function (inputDate) {
-    inputDate.addCalendar()
+    inputDate.addCalendar();
   })
 })
